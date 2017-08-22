7fe4c171fa026f5471aab1169be54c9a82757fe3
andfroswe <gummianka88@gmail.com>
2017-08-22 21:39:57 +0200

    Quick and easy annoyotron that works but no sleep mode so probably not very long lived on battery

--- annoyotron.c
+++ annoyotron.c
@@ -4,12 +4,15 @@
  * annoy people.
  */
 
+#define MAX_WAIT_MINUTES 10 // The maximum amount of minutes to wait before new tone
+
 #include <avr/io.h>
+#define F_CPU 1000000UL
+#include <util/delay.h>
 
 int main(int argc, char *argv[])
 {
-    DDRD |= 1 << PD6;
-    /*PORTD |= 1 << PD6;*/
+    DDRD |= 1 << PD6; // Enable PWM oscillator flag
 
     // Setup PWM
     /*TCCR0B |= _BV(CS00) | _BV(CS02); // Clock source, prescaler 1024*/
@@ -17,12 +20,23 @@ int main(int argc, char *argv[])
     TCCR0A |= _BV(WGM00) | _BV(WGM01); // Wave generator mode Fast PWM
     TCCR0B |= _BV(WGM02); // Toggle on compare match
     TCCR0A |= _BV(COM0A0); // Toggle on compare match
-    OCR0A = 50;
+    OCR0A = 40; // This yields approx 10 kHz signal
 
     // Main loop
+    int wait_minutes = 5;
     while(1)
     {
-
+        // Make a tone
+        _delay_ms(500);
+        TCCR0B &= ~_BV(WGM02); // PWM off
+        // Wait until next tone
+        int i;
+        for (i = 0; i < wait_minutes; ++i)
+        {
+            _delay_ms(60000);
+        }
+        TCCR0B |= _BV(WGM02); // PWM on
+        wait_minutes = ++wait_minutes%MAX_WAIT_MINUTES;
     }
 
     return 0;
